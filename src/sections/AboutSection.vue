1e486dc851674d72db43670948287b9176af9b3b
Felipe Caldas <caldas.rj@gmail.com>
2025-04-29 13:12:16 -0300

    Add files via upload

--- src/sections/AboutSection.vue
+++ src/sections/AboutSection.vue
@@ -1,33 +1,19 @@
 <template>
-    <!-- About section start -->
-    <section id="about-section" class="px-12 lg:px-32 py-16 border-t border-stone-500 relative">
-        <!-- Main text start -->
-        <h3 class="uppercase text-3xl mb-4 font-semibold ">
-            Apresentação
-        </h3>
-        <p class="w-full text-stone-500 mb-2">
-            Com mais de 15 anos de experiência em desenvolvimento front-end, sou especializado em criar interfaces de
-            usuário intuitivas e responsivas. Colaboração com equipes multidisciplinares.<br>
-            Vasta experiência com (CMS) e comércio eletrônico. Colaborador de projetos nacionais e internacionais,
-            atuando em e-commerce, startups e agências.
-        </p>
-        <!-- Main text end -->
-
-        <!-- Social links start -->
-        <div class="flex mt-3 gap-5 keyboard">
-            <i class="fa-brands fa-html5 socialIcon2 key"></i>
-            <i class="fa-brands fa-css3 socialIcon2 key"></i>
-            <i class="fa-brands fa-js socialIcon2 key"></i>
-            <i class="fa-brands fa-bootstrap socialIcon2 key"></i>
-            <i class="fa-brands fa-vuejs socialIcon2 key"></i>
-            <i class="fa-brands fa-wordpress socialIcon2 key"></i>
-            <i class="fa-brands fa-magento socialIcon2 key"></i>
-            <i class="fa-brands fa-shopify socialIcon2 key"></i>
-        </div>
-        <!-- Social links end -->
-
-    </section>
-    <!-- About section end -->
+  <!-- About section start -->
+  <section id="about-section" class="px-12 lg:px-32 py-16 border-t border-stone-500 relative">
+    <!-- Main text start -->
+    <h3 class="uppercase text-3xl mb-4 font-semibold ">
+      Apresentação
+    </h3>
+    <p class="w-full text-stone-500 mb-2">
+      Com mais de 15 anos de experiência em desenvolvimento front-end, sou especializado em criar interfaces de
+      usuário intuitivas e responsivas. Colaboração com equipes multidisciplinares.<br>
+      Vasta experiência com (CMS) e comércio eletrônico. Colaborador de projetos nacionais e internacionais,
+      atuando em e-commerce, startups e agências.
+    </p>
+    <!-- Main text end -->
+  </section>
+  <!-- About section end -->
 </template>
 
 <script>
@@ -37,7 +23,6 @@ export default {
 </script>
 
 <style>
-
 .key {
   font-size: 15vw;
   display: inline-block;
@@ -48,88 +33,104 @@ export default {
 
 
 @keyframes pressDown1 {
+
   30%,
   40%,
   100% {
     transform: translateY(0);
   }
+
   35% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown2 {
+
   70%,
   80%,
   100% {
     transform: translateY(0);
   }
+
   75% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown3 {
+
   30%,
   40%,
   100% {
     transform: translateY(0);
   }
+
   35% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown4 {
+
   40%,
   50%,
   100% {
     transform: translateY(0);
   }
+
   45% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown5 {
+
   20%,
   30%,
   100% {
     transform: translateY(0);
   }
+
   25% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown6 {
+
   60%,
   70%,
   100% {
     transform: translateY(0);
   }
+
   65% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown7 {
+
   10%,
   20%,
   100% {
     transform: translateY(0);
   }
+
   15% {
     transform: translateY(10px);
   }
 }
 
 @keyframes pressDown8 {
+
   35%,
   45%,
   100% {
     transform: translateY(0);
   }
+
   40% {
     transform: translateY(10px);
   }
@@ -168,5 +169,4 @@ export default {
 .key:nth-child(8) {
   animation: pressDown8 3.2s infinite;
 }
-
 </style>
