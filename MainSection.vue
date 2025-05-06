841fd932a9f50c048da61b51a92dfe2d458c4333
Felipe Caldas <caldas.rj@gmail.com>
2025-05-06 11:15:27 -0300

    Add files via upload

---
+++ MainSection.vue
@@ -0,0 +1,54 @@
+<template>
+	<!-- Main section start -->
+	<section
+		class="min-h-screen w-full px-4 sm:px-8 md:px-12 lg:px-32 py-16 md:py-32 mb-8 flex flex-col md:flex-row items-center">
+		<div class="md:w-1/2 flex flex-col justify-center text-center md:text-left">
+			<!-- Main text start -->
+			<h3 class="uppercase text-4xl sm:text-5xl xl:text-7xl mb-4 font-semibold">
+				front-end UI/UX
+			</h3>
+			<p class="text-stone-500 max-w-md mx-auto md:mx-0">
+				Especialista em UX e usabilidade em e-commerce, com expertise na criação e otimização de lojas virtuais.
+				Experiente em design e avaliação de interfaces, garantindo soluções intuitivas e eficientes. Foco em
+				maximizar conversões e engajamento do usuário. </p>
+			<!-- Main text end -->
+
+			<!-- Social links start -->
+			<div class="flex justify-center md:justify-start mt-6 gap-6">
+				<a href="https://www.linkedin.com/in/felipe-caldas37/" target="_blank">
+					<i class="fa-brands fa-linkedin socialIcon"></i>
+				</a>
+				<a href="https://github.com/felipecm37" target="_blank">
+					<i class="fa-brands fa-github socialIcon"></i>
+				</a>
+				<a href="https://x.com/HostelinRio" target="_blank">
+					<i class="fa-brands fa-x socialIcon"></i>
+				</a>
+				<a href="https://www.instagram.com/stories/felipecm37/" target="_blank">
+					<i class="fa-brands fa-instagram socialIcon"></i>
+				</a>
+				<a href="https://wa.me/5521987654321" target="_blank">
+					<i class="fa-brands fa-whatsapp socialIcon"></i>
+				</a>
+			</div>
+			<!-- Social links end -->
+		</div>
+
+		<!-- Photographer image start -->
+		<div class="relative md:w-1/2 flex justify-center mt-6">
+			<img src="@/assets/image.jpg" alt="img" class="w-full max-h-[300px] object-cover rounded-lg">
+		</div>
+		<!-- Photographer image end -->
+
+
+	</section>
+	<!-- Main section end -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
