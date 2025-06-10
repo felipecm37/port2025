c0b8aa3ac177c2d74220b024554a4b232f5784ae
Felipe Caldas <caldas.rj@gmail.com>
2025-06-10 13:54:54 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TheCaldas - Portfolio 2025
+### Original design: https://dribbble.com/shots/16247167-photographer-landing-page
+### by AR Shakir
+
+![Mobile view](./img/mobile.png)
+![Desktop view](./img/desktop.png)
---
+++ src/components/FooterElement.vue
@@ -0,0 +1,44 @@
+<template>
+	<!-- Footer start -->
+	<footer class="bg-white dark:bg-gray-900" style="padding: 20px;">
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 Felipe Caldas.
+			</span>
+			<!-- Social links start -->
+			<div class="flex mt-3 gap-8">
+				<a href="https://www.linkedin.com/in/felipe-caldas37/" target="_blank">
+					<i class="fa-brands fa-linkedin socialIcon"></i>
+				</a>
+				<a href="https://github.com/felipecm37" target="_blank">
+					<i class="fa-brands fa-github socialIcon"></i>
+				</a>
+				<a href="https://x.com/HostelinRio" target="_blank">
+					<i class="fa-brands fa-x socialIcon"></i>
+				</a>
+				<a href="https://www.instagram.com/stories/felipecm37/" target="_blank">
+					<i class="fa-brands fa-instagram socialIcon"></i>
+				</a>
+				<a href="https://wa.me/5521987654321" target="_blank">
+					<i class="fa-brands fa-whatsapp socialIcon"></i>
+				</a>
+			</div>
+			<!-- Social links end -->
+		</div>
+	</footer>
+
+	<div class="fixed bottom-4 right-4 flex items-center">
+		<a href="javascript:window.scrollTo(0,0)">
+			<i
+				class="fa-solid fa-arrow-up transition text-stone-100 text-xl bg-blue-500 rounded-full shadow p-4 hover:-translate-y-2 hover:shadow-xl"></i>
+		</a>
+	</div>
+	<!-- Footer end -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
